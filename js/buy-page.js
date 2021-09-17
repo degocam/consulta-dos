5962f50a3d37a408678b51d8af7b2b3a4d002b7f
degocam <85023334+degocam@users.noreply.github.com>
2021-09-17 00:13:02 -0300

    cambio final

--- js/buy-page.js
+++ js/buy-page.js
@@ -1,10 +1,14 @@
-$(document).ready(function(){
-    console.log('Listo para cargar');
-});
+function precioEntradas (a,b){
+  let totalEntradas = a * b;
+  console.log(totalEntradas);
+  return totalEntradas;
+};
+function renderPeli() {
+const nombrePelicula = localStorage.getItem('pelicula') ? localStorage.getItem('pelicula') : '';
+const peliculas = localStorage.getItem('peliculas') ? JSON.parse(localStorage.getItem('peliculas')) : [];
 
-//Esta es la sección que brinda información de la película
-
-$(".infoPeli").append(
+if(!nombrePelicula || peliculas.length === 0) {
+  $(".infoPeli").append(
     `<img src="resources/unavailable.svg" alt="">
     <h2>Nombre Película</h2>
     <p>Duis ullamco irure ex fugiat enim incididunt esse magna officia aliquip ipsum occaecat. 
@@ -13,90 +17,81 @@ $(".infoPeli").append(
     Ex in ullamco aliqua laboris ex dolor ut duis dolor ipsum aliqua laboris fugiat.</p>
     <h3>Duración: 115 minutos</h3>
     <h3>Director: Fulano Mengano</h3>`);
+  return;
+}
+const pelicula = peliculas.find(peli => peli.nombre === nombrePelicula);
+$(".infoPeli").html(
+  `<img src="${pelicula.img}" alt="${pelicula.nombre}">
+  <h2>${pelicula.nombre}</h2>
+  <p>${pelicula.descripcion}</p>
+  <h3>Duración: ${pelicula.duracion}</h3>
+  <h4>Director: ${pelicula.director}</h3>`);
+const horarios = pelicula.horario.split(', ');
+let horasBtn = '';
+horarios.forEach(hora => {
+  horasBtn += `
+    <button class="btn-horarios" value="${hora}">${hora}</button>
+  `;
+  return horasBtn;
+})
 
-
-$(".btn-horarios").click((e)=>{
+$('#horariosPelis').html(horasBtn)
+}
+$(document).ready(function(){
+renderPeli();
+  const inputsCompra = document.querySelectorAll('.input-compra');
+  const form = document.querySelector('#buyTicket');
+  const numeroEntradas = document.getElementById("numEntradas");
+  const modal = document.getElementById("modalTotal");
+  const span = document.getElementsByClassName("close")[0];
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  $(".btn-horarios").click((e)=>{
     e.preventDefault();
     $("#qEntradas").slideDown();
     $("#divCard").slideDown();
-
-    // let horaElegida = e.target.value;
-
-    // return horaElegida;
-});
-
-// let horaPelis = document.getElementById("horariosPelis");
-
-// $(".btn-horarios").click(function(e){
-//   e.preventDefault();
-
-// });
-
-$("#nombreComp").change(function(e){
-  e.preventDefault();
-  let nombreComp = e.target.value;
-
-  return nombreComp;
-});
-
-$("#correoComp").change(function(e){
-  e.preventDefault();
-  let correoComp = e.target.value;
-
-  return correoComp;
-});
-
-$("#telNumber").change(function(e){
-  e.preventDefault();
-  let telNumber = e.target.value;
-
-  return telNumber;
-});
-
-// Esta es la función que calcula el total de las entradas
-let numeroEntradas = document.getElementById("numEntradas");
-
-let cantEntradas = $("#comprar").click(function(e){
-    e.preventDefault();
-    let cantidadEntradas = parseInt(numeroEntradas.value);
-    // let horaFuncion = String(horaElegida.value);
-    let emailComprador = String(correoComp.value);
-    let nombreComprador = String(nombreComp.value);
-    let telefonoComprador = String(telNumber.value);
-    const total = precioEntradas(cantidadEntradas, 2990);
-
-    $(".modalContenido").append(
-        `<h2>${nombreComprador}</h2>
-        <p>Haz comprado ${cantidadEntradas} entradas para la función de las </p>
-        <p>Tu código para retirar en boletería llegará al correo ${emailComprador}</p>
-        <p>y al teléfono ${telefonoComprador}</p>
-        <h3>Total:${total}</h3>`
-    )
-});
-
-function precioEntradas (a,b){
-    let totalEntradas = a * b;
-    console.log(totalEntradas);
-    return totalEntradas;
-};
-
-//Esta es la función que despliega el modal
-var modal = document.getElementById("modalTotal");
-
-var btn = document.getElementById("comprar");
-
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+    let horaElegida = e.target.value;
+    console.log(horaElegida);
+  });
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
   }
-} 
+  let dataComprador = {
+    name:'',
+    email:'',
+    telefono: '',
+    entradas: ''
+  }
+  inputsCompra.forEach(input => {
+    input.addEventListener('change', (e) => {
+      dataComprador = {
+        ...dataComprador,
+        [e.target.name]: e.target.value
+      }
+    })
+  });
+  numeroEntradas.addEventListener('change', e => {
+    dataComprador = {
+      ...dataComprador,
+      [e.target.name]:e.target.value
+    }
+  });
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log(dataComprador);
+    const total = precioEntradas(parseInt(dataComprador.entradas), 2990);
+    modal.style.display = "block";
+    $(".modalContenido").html(
+      `<h2>${dataComprador.name}</h2>
+      <p>Haz comprado ${dataComprador.entradas} entradas para esta función.</p>
+      <p>Tu código para retirar en boletería llegará al correo ${dataComprador.email}.</p>
+      <p>y al teléfono ${dataComprador.telefono}.</p>
+      <h3>Total:${total}</h3>`
+    )
+    form.reset();
+  })
+});
